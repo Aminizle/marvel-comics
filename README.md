c913af24f5197e636688d88e4bcd8d122e091f0b
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-18 16:26:42 -0500

    added login and register pages

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # marvel-comics
 https://developer.marvel.com
+
+
+Node.js, Express & passport tutorial
+https://www.youtube.com/watch?v=-RCnNyD0L-s
+
--- server.js
+++ server.js
@@ -7,4 +7,17 @@ app.get('/', (req, res) => {
    res.render('index.ejs', { name: 'Ameen'}) 
 })
 
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs') 
+ })
+
+
+ app.get('/register', (req, res) => {
+    res.render('register.ejs') 
+ })
+
+
+
+
 app.listen(3000)
